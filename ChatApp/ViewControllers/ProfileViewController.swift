bdf5706f97b20532fe84b9987b09a94993fa6727
Liyana <liyanas2003@gmail.com>
2025-07-25 17:02:52 +0530

    improved profileViewController userInterface

--- ChatApp/ViewControllers/ProfileViewController.swift
+++ ChatApp/ViewControllers/ProfileViewController.swift
@@ -17,7 +17,19 @@ class ProfileViewController: UIViewController {
     @IBOutlet weak var usernameLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        avatarImageView.image = UIImage(systemName: "person.fill")
+        avatarImageView.tintColor = UIColor.black
+        avatarImageView.backgroundColor = UIColor.lightGray
+        let avatarTap = UITapGestureRecognizer(target: self, action: #selector(presentAvatarOptions))
+        avatarImageView.isUserInteractionEnabled = true
+        avatarImageView.addGestureRecognizer(avatarTap)
+        
+        
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        containerView.layer.cornerRadius = 8
+        avatarImageView.layer.cornerRadius = avatarImageView.frame.width / 2
     }
     func logout() {
         do {
@@ -31,7 +43,29 @@ class ProfileViewController: UIViewController {
             presentErrorAlert(title: "Logout Failed", message: "something went wrong with logout.Please try again later")
             
         }
+        
     }
+    @objc func presentAvatarOptions() {
+        let avatarOptionSheet = UIAlertController(title: "Change avatar", message: "select an option", preferredStyle: .actionSheet)
+        let cameraAction = UIAlertAction(title: "Camera", style: .default) { _ in
+        }
+        let photoAction = UIAlertAction(title: "Photo Library", style: .default) { _ in
+        }
+        let deleteAction = UIAlertAction(title: "Delete Avatar", style: .destructive) { _ in
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        avatarOptionSheet.addAction(cameraAction)
+        avatarOptionSheet.addAction(photoAction)
+        avatarOptionSheet.addAction(deleteAction)
+        avatarOptionSheet.addAction(cancelAction)
+        present(avatarOptionSheet, animated: true)
+        
+        
+    }
+    @IBAction func dismissButtonTapped(_ sender: Any) {
+        dismiss(animated: true)
+    }
+   
     
 
     @IBAction func logoutButtonTapped(_ sender: Any) {
